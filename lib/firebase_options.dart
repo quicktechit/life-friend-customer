5f669aab0a6ab83a043ae5d050058a3227391b5c
nrkhan-223 <redoykhan8141@gmail.com>
2026-04-12 14:24:32 +0600

    package name change and upload

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -51,7 +51,7 @@ class DefaultFirebaseOptions {
 
   static const FirebaseOptions android = FirebaseOptions(
     apiKey: 'AIzaSyCDIWhZazysSWz5x4tsziL0T-5d4TnM6QY',
-    appId: '1:823258685691:android:4ede81e32e395a19fa105e',
+    appId: '1:823258685691:android:1b7a1aac87dae9b2fa105e',
     messagingSenderId: '823258685691',
     projectId: 'riderrapp-5cef9',
     storageBucket: 'riderrapp-5cef9.firebasestorage.app',
@@ -59,7 +59,7 @@ class DefaultFirebaseOptions {
 
   static const FirebaseOptions ios = FirebaseOptions(
     apiKey: 'AIzaSyCw9rAg2MA4YpXn9EhcJ8eto6KYVFZ45p4',
-    appId: '1:823258685691:ios:a5f520cb84aa1408fa105e',
+    appId: '1:823258685691:ios:8d8159921283f112fa105e',
     messagingSenderId: '823258685691',
     projectId: 'riderrapp-5cef9',
     storageBucket: 'riderrapp-5cef9.firebasestorage.app',
